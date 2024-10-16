fe2772898c80bbec0c7be8e864ed6fca63a62897
iScarcy <scarcella.giuseppe@gmail.com>
2024-10-16 23:59:12 +0200

    models

---
+++ Source.py
@@ -0,0 +1,42 @@
+import datetime
+import decimal
+
+class SourceRow:
+    
+    tipo : str
+    verbale_id : str
+    cognome : str
+    nome : str
+    sesso : str
+    dataNascita : datetime 
+    comuneNascita : str
+    statoNascita : str
+    codiceFiscale : str
+    residenza : str
+    dataViolazione : datetime
+    targa : str
+    dataNotifica : datetime
+    annoRiferimento : str
+    importoSansioniRuolo : decimal
+    importoSpese : decimal
+
+    def __init__(self, rowSource : set):
+        self.read_data(row=rowSource)
+
+    def read_data(self,row:set):
+        self.tipo        =   row["tipo"]
+        self.verbale_id  =   row["verbale_id"]
+        self.cognome     =   row["cognome"] 
+        self.nome        =   row["nome"] 
+        self.sesso       =   row["sesso"]
+        self.dataNascita =   row["data_nascita"]  
+        self.comuneNascita =   row["comune_mascita"] 
+        self.statoNascita =   row["stato_nascita"]   
+        self.codiceFiscale = row["codice_fiscale"]
+        self.residenza = row["residenza"]
+        self.dataViolazione = row["data_violazione"]
+        self.targa = row["targa"]
+        self.dataNotifica = row["data_notifica"]
+        self.annoRiferimento = row["anno_riferimento"]
+        self.importoSansioniRuolo = row["importo_sansioni_ruolo"]
+        self.importoSpese = row["importo_spese"]
--- Verbale.py
+++ Verbale.py
@@ -1,12 +1,64 @@
+import decimal
+from Source import SourceRow
+
 class VerbaleRow:
-    tipo = ""
-    verbale_id = ""
-    cognome = ""
+    protocollo:str
+    annoDebito:int
+    codTipoAtto:str
+    dataAtto:int 
+    dataNotificaAtto:int
+    estremiAtto:str
+    motivazioneAtto:str
+    descrizioneAtto:str
+    canale:str
+    soggetto1_CodiceFiscale:str 
+    soggetto1_Cognome:str 
+    soggetto1_Nome:str  
+    soggetto1_IndirizzoCodiceBelfiore:str 
+    soggetto1_Indirizzo:str 
+    soggetto1_IndirizzoNumero:str 
+    soggetto1_IndirizzoComplementi:str 
+    soggetto1_IndirizzoLocalita:str 
+    soggetto1_IndirizzoCAP:str  
+    soggetto1_IndirizzoComune:str  
+    soggetto1_IndirizzoSiglaProvincia:str  
+    soggetto1_NascitaCodiceBelfiore:str  
+    soggetto1_NascitaData:int  
+    soggetto1_NascitaComune:str  
+    soggetto1_NascitaSiglaProvincia:str  
+    soggetto1_Natura:str  
+    soggetto1_Sesso:str  
 
-    def __init__(self, rowSource : set):
-        self.read_data(row=rowSource)
+    soggetto2_CodiceFiscale:str 
+    soggetto2_Cognome:str 
+    soggetto2_Nome:str  
+    soggetto2_IndirizzoCodiceBelfiore:str 
+    soggetto2_Indirizzo:str 
+    soggetto2_IndirizzoNumero:str 
+    soggetto2_IndirizzoComplementi:str 
+    soggetto2_IndirizzoLocalita:str 
+    soggetto2_IndirizzoCAP:str  
+    soggetto2_IndirizzoComune:str  
+    soggetto2_IndirizzoSiglaProvincia:str  
+    soggetto2_NascitaCodiceBelfiore:str  
+    soggetto2_NascitaData:int  
+    soggetto2_NascitaComune:str  
+    soggetto2_NascitaSiglaProvincia:str  
+    soggetto2_Natura:str  
+    soggetto2_Sesso:str  
+    tributo1_codice:str
+    tributo1_totale:decimal
+    tributo2_codice:str
+    tributo2_totale:decimal
+    tributo3_codice:str
+    tributo3_totale:decimal
+ 
 
-    def read_data(self,row:set):
-        self.tipo       =   row["tipo"]
-        self.verbale_id =   row["verbale_id"]
-        self.cognome    =   row["cognome"] 
+    def __init__(self, row : SourceRow):
+        self.protocollo = row.verbale_id
+        self.annoDebito = row.verbale_id #TODO PRENDERE GLI ULTRIMI 4 CARATTERI
+        self.codTipoAtto = "VE"
+        
+
+    
+        
